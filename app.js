7bfae3e1244e8a4d0a60849969979ed1a865be80
Kyle Michael Pingue <user.name>
2026-04-29 16:27:01 -0400

    fixing app.js for express-handlbars

--- app.js
+++ app.js
@@ -18,8 +18,13 @@
 
 import configRoutes from './routes/index.js';
 import express from 'express';
+import { engine } from "express-handlebars";
 
 const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 app.use('public', express.static('public'));
 app.use(express.json());
