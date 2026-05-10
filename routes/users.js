c3d220968dd617f083c8dc5ee42cdc4bcf339816
annatolbanana <anna.tolmanov@gmail.com>
2026-05-10 00:44:05 -0400

    check if at least 13 y.o at signup

--- routes/users.js
+++ routes/users.js
@@ -82,10 +82,13 @@ router.post('/signup', async (req, res) => {
   const { firstName, lastName, age, email, username, password, borough } = req.body;
 
   try {
+    const parsedAge = parseInt(age);
+    if (isNaN(parsedAge)) throw 'Age must be a number';
+    if (parsedAge < 13) throw 'You must be at least 13 years old to create an account';
     const newUser = await userData.createUser({
       firstName,
       lastName,
-      age: parseInt(age),
+      age: parsedAge,
       email,
       username,
       password,
